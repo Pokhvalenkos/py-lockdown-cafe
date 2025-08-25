a4b12c5bf758e25ce1f11d807d4335ccdedd16fe
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2025-08-25 12:55:38 +0200

    Fix problems according to review request

--- app/cafe.py
+++ app/cafe.py
@@ -13,12 +13,12 @@ class Cafe:
 
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
-            raise NotVaccinatedError
+            raise NotVaccinatedError("Visitor is not vaccinated")
 
         if visitor["vaccine"]["expiration_date"] < date.today():
-            raise OutdatedVaccineError
+            raise OutdatedVaccineError("Visitors vaccination is outdated")
 
-        if visitor["wearing_a_mask"] is False:
-            raise NotWearingMaskError
+        if not visitor.get("wearing_a_mask", False):
+            raise NotWearingMaskError("Visitor is not wearing a mask")
 
         return f"Welcome to {self.name}"
--- app/errors.py
+++ app/errors.py
@@ -1,18 +1,45 @@
 class VaccineError(Exception):
+    def __init__(
+        self,
+        message: str = "Visitors hav problems with their vaccine"
+    ) -> None:
+        self.message = message
+
     def __str__(self) -> str:
-        return "Visitors hav problems with their vaccine"
+        return self.message
 
 
 class NotVaccinatedError(VaccineError):
+    def __init__(
+            self,
+            message: str = "NotVaccinatedError, "
+                           "some visitor/s do not have vaccination"
+    ) -> None:
+        self.message = message
+
     def __str__(self) -> str:
-        return "NotVaccinatedError, some visitor/s do not have vaccination"
+        return self.message
 
 
 class OutdatedVaccineError(VaccineError):
+    def __init__(
+            self,
+            message: str = "OutdatedVaccineError, "
+                           "some visitor/s have expired vaccination"
+    ) -> None:
+        self.message = message
+
     def __str__(self) -> str:
-        return "OutdatedVaccineError, some visitor/s have expired vaccination"
+        return self.message
 
 
 class NotWearingMaskError(Exception):
+    def __init__(
+            self,
+            message: str = "NotWearingMaskError, "
+                           "some visitor/s do not wear a mask"
+    ) -> None:
+        self.message = message
+
     def __str__(self) -> str:
-        return "NotWearingMaskError, some visitor/s do not wear a mask"
+        return self.message
